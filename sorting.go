3bdcfef5fc379fee67cce817aa2493c9bfbd7ff9
Shahriar Ahmed (Sazid) <shahriar.ashrafi@pathao.com>
2022-10-04 17:19:51 +0600

    bubble sort added

---
+++ sorting.go
@@ -0,0 +1,15 @@
+package algo
+
+import "golang.org/x/exp/constraints"
+
+func BubbleSort[T constraints.Ordered](slice []T) {
+	for i := 0; i < len(slice); i++ {
+		for j := 0; j < len(slice)-1-i; j++ {
+			if slice[j] > slice[j+1] {
+				temp := slice[j]
+				slice[j] = slice[j+1]
+				slice[j+1] = temp
+			}
+		}
+	}
+}
---
+++ sorting_test.go
@@ -0,0 +1,28 @@
+package algo
+
+import (
+	"testing"
+
+	. "github.com/stretchr/testify/require"
+	"golang.org/x/exp/constraints"
+)
+
+func isSorted[T constraints.Ordered](t *testing.T, s []T) {
+	for i := 0; i < len(s)-1; i++ {
+		LessOrEqual(t, s[i], s[i+1])
+	}
+}
+func TestSorting(t *testing.T) {
+	s := []int{1, 3, 42, 22, 3, 3, 1, 2}
+	BubbleSort(s)
+	isSorted(t, s)
+
+	ss := []string{"af", "sdj", "sdf", "23", "ter", "aa", "b"}
+	BubbleSort(ss)
+	isSorted(t, ss)
+
+	s = []int{}
+	BubbleSort(s)
+	isSorted(t, s)
+
+}
